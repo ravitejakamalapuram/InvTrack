68a56168be1ad1afc756943c15a92e51185ae307
Raviteja Kamalapuram <rkamalapuram@tekion.com>
2026-01-30 21:33:12 +0530

    feat: Enable screenshots - remove FLAG_SECURE restriction

--- android/app/src/main/kotlin/com/invtracker/inv_tracker/MainActivity.kt
+++ android/app/src/main/kotlin/com/invtracker/inv_tracker/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.invtracker.inv_tracker
 
-import android.view.WindowManager
 import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 
@@ -9,13 +8,6 @@ class MainActivity : FlutterFragmentActivity() {
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
 
-        // Prevent screenshots, screen recording, and app switcher previews in release builds
-        // to protect sensitive financial data.
-        // Note: Using ApplicationInfo.FLAG_DEBUGGABLE instead of BuildConfig.DEBUG because
-        // BuildConfig may not be generated before Kotlin compilation in some build configurations.
-        val isDebuggable = (applicationInfo.flags and android.content.pm.ApplicationInfo.FLAG_DEBUGGABLE) != 0
-        if (!isDebuggable) {
-            window.addFlags(WindowManager.LayoutParams.FLAG_SECURE)
-        }
+        // Screenshot restriction disabled - users can now take screenshots
     }
 }
