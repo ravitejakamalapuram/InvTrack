51f49f02411f78e1aa48c1b348cd966249c14d92
Raviteja Kamalapuram <rkamalapuram@tekion.com>
2026-03-09 10:53:06 +0530

    fix: Suppress sealed class warnings in test file
    - Add ignore_for_file directive for subtype_of_sealed_class
    - Follows pattern from other test files (version_check_service_test.dart)
    - All warnings now fixed (0 warnings, 14 info messages)
    - Ready for CI

--- test/core/services/currency_conversion_service_test.dart
+++ test/core/services/currency_conversion_service_test.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: subtype_of_sealed_class
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
