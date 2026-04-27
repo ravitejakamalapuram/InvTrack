19ffa1ac13fd167e869752dae8dd1cfe3edf7fa5
Raviteja Kamalapuram <raviteja.kamalapuram@gmail.com>
2026-04-27 15:44:15 +0530

    fix: Add serverClientId for Google Sign-In on Android
    Fixes GoogleSignInException: serverClientId must be provided on Android (55 crash events, 18 users affected)
    
    Changes:
    - Added serverClientId (Web Client ID) to GoogleSignIn.instance.initialize() for Android/iOS
    - Using Web OAuth Client ID from google-services.json: 784857267556-dkge5l37c12n1ohrljle8s6nim0cgq84
    
    Root Cause:
    - google_sign_in v7+ REQUIRES serverClientId for Android
    - Previous code incorrectly stated it was deprecated and did not pass it
    - This caused clientConfigurationError crashes when users tried to sign in
    
    Impact:
    - Fixes 55 crash events affecting 18 users
    - Fixes account linking failures
    - Fixes re-authentication failures
    
    References:
    - Crashlytics Issue: 9dfdf1143e4d5e88cbfe9a9d91440e44
    - GitHub Issue: https://github.com/flutter/flutter/issues/172073
    
    Verified:
    - flutter analyze: No issues found
    - Correct Web Client ID from google-services.json (client_type: 3)
    - Code complies with InvTrack Enterprise Rules

--- lib/features/auth/presentation/providers/auth_provider.dart
+++ lib/features/auth/presentation/providers/auth_provider.dart
@@ -21,10 +21,14 @@ final googleSignInInitializedProvider = FutureProvider<void>((ref) async {
           '20057918856-r6qh2gt5eqk2o3oiq8fkt8pgfhquja6a.apps.googleusercontent.com',
     );
   } else {
-    // Android/iOS: Don't pass serverClientId - it's deprecated in v7 and causes NETWORK_ERROR
-    // Firebase Auth only needs idToken from GoogleSignInAccount.authentication
-    // The Web Client ID in google-services.json is automatically used
-    await GoogleSignIn.instance.initialize();
+    // Android/iOS: MUST pass serverClientId (Web Client ID) for google_sign_in v7+
+    // This is the Web OAuth Client ID from google-services.json (client_type: 3)
+    // Required to fix GoogleSignInException: "serverClientId must be provided on Android"
+    // See: https://github.com/flutter/flutter/issues/172073
+    await GoogleSignIn.instance.initialize(
+      serverClientId:
+          '784857267556-dkge5l37c12n1ohrljle8s6nim0cgq84.apps.googleusercontent.com',
+    );
   }
 });
 
